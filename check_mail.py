f138faa75654d9ba8d22d67739d2eced45bc6b41
John Brennan <brennanj414@gmail.com>
2023-04-05 22:00:48 -0600

    begin python script, setup db

---
+++ check_mail.py
@@ -0,0 +1,22 @@
+from tinydb import TinyDB, Query
+db = TinyDB('db.json')
+
+def retrieve():
+  f = db.all()
+  print(f)
+
+retrieve()
+
+# Every minute
+
+# Check for mail (laser)
+
+# Is flag raised?
+
+# Check to see if we've texted Cole today
+
+# Call rest api
+
+# If 200, add to tinydb
+
+ 
